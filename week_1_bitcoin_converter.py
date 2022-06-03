374aa2a17be39fa1a43c6ec00b4555895aed6093
qiaoqiao124 <101111340+qiaoqiao124@users.noreply.github.com>
2022-06-03 12:30:43 -0700

    test file-python

---
+++ week_1_bitcoin_converter.py
@@ -0,0 +1,5 @@
+
+print("As of 8/27/21 at 8:13 pm, bitcoin is currently trading at $48952.90 per bitcoin.")
+input_ = input("Enter the bitcoin amount: ")
+amount = float(input_) * 48952.90
+print("That is worth ", amount, "us dollars.")
